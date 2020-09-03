2cde73aa0795e16d9e1a0aa9632068408383a246
zeroing2 <zeroing2@gmail.com>
2020-09-03 21:51:40 +0900

    + 회원 웹 기능 (등록)

--- src/main/java/zeroing2/hellospring/controller/MemberController.java
+++ src/main/java/zeroing2/hellospring/controller/MemberController.java
@@ -2,6 +2,9 @@ package zeroing2.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import zeroing2.hellospring.domain.Member;
 import zeroing2.hellospring.service.MemberService;
 
 @Controller
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/zeroing2/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package zeroing2.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
