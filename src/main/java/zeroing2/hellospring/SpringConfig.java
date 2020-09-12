45d9e0d63e1b7c52413b1b2b540b725571381c6d
zeroing2 <zeroing2@gmail.com>
2020-09-13 00:15:51 +0900

    + 순수 JDBC 코드

--- src/main/java/zeroing2/hellospring/SpringConfig.java
+++ src/main/java/zeroing2/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package zeroing2.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import zeroing2.hellospring.repository.JdbcMemberRepository;
 import zeroing2.hellospring.repository.MemberRepository;
 import zeroing2.hellospring.repository.MemoryMemberRepository;
 import zeroing2.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
